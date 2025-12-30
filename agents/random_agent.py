c3a755ccdf7e13fabe7c8e19dc63196ab69b661e
Tom-Boyce <163887585+Tom-Boyce@users.noreply.github.com>
2025-12-30 12:57:47 +0000

    Add pretrained Space Invaders models and remove obsolete files. Add notebook for comparison of agents.

--- agents/random_agent.py
+++
@@ -1,9 +0,0 @@
-from .base_agent import Agent
-import gymnasium as gym
-
-class RandomAgent(Agent):
-    def __init__(self, env: gym.Env):
-        super().__init__(env)
-
-    def act(self, obs):
-        return self.action_space.sample()
